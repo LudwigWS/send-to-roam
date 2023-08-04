1c696e8a5efe4ea2512a192b7e3a33bc7791249b
LudwigWS <LudwigWS@outlook.com>
2023-08-04 16:54:30 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # send-to-roam
 Send to Roam Research using Shortcuts and Scriptable
+
+## Requirements
+
+- iOS App: Scriptable, a free App used for sending a network to Roam Research
+- Install Send to Roam.scriptable in iOS
---
+++ send-to-roam.js
@@ -0,0 +1,58 @@
+
+// this is a Scriptable script
+
+let url = ""
+// 
+// new Request()
+
+// let alert = new Alert()
+// alert.message = args.shortcutParameter
+// 
+// alert.message = "jjj"
+// 
+// alert.presentAlert()
+
+// QuickLook.present(args.shortcutParameter)
+
+let headerToken = args.plainTexts[0]
+let endpoint = args.urls[0]
+let body = args.shortcutParameter
+
+// let notification = new Notification()
+// notification.body = JSON.stringify(body)
+// notification.schedule()
+
+let request = new Request(endpoint)
+request.headers = {
+  "accept": "application/json",
+  "Content-Type": "application/json",
+  "Authorization": headerToken,
+}
+request.body = JSON.stringify(body)
+request.method = "POST"
+request.allowInsecureRequest = true
+request.onRedirect = 
+ (request) => {
+  request.headers = {...{
+  "accept": "application/json",
+  "Content-Type": "application/json",
+  "Authorization": headerToken,
+  }}
+  
+  request.body = JSON.stringify(body)
+  return request
+}
+
+let promise = request.loadJSON()
+
+// promise.then(() => {
+//   let response = request.response
+//   let notification = new Notification()
+//   notification.body = JSON.stringify("response")
+//   notification.schedule()
+// 
+// })
+
+
+
+Script.complete()
